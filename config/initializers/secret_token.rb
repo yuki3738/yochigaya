8fd59e4ae7dd06e8d202277fbe94be1807a5fba7
Toru KAWAMURA <tkawa@4bit.net>
2015-02-14 16:41:47 +0900

    :heavy_dollar_sign: rails new yochigaya -T -B

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yochigaya::Application.config.secret_key_base = '4c1026a29aa11f86470adb0905f6765e7c145c518d7461c00e3addbe683567c75b1b84b2c38f3b4352cae774e466002f37a15c8795fc5437a87296ec43c9e14d'
