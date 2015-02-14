62f08080468554f009b6ef7e351dc26a779a6b5c
Toru KAWAMURA <tkawa@4bit.net>
2015-02-14 17:50:46 +0900

    static_pages#home をルートページに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Yochigaya::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
+  root 'static_pages#home'
+  get 'static_pages/help'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
